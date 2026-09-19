8bb5b5797af42d816779464f538133224f5fdce0
Yogeshwaran <yogim3110@gmail.com>
2026-09-19 22:36:06 +0530

    Initialize Spring Boot project

---
+++ src/main/java/com/codereview/agent/CodereviewApplication.java
@@ -0,0 +1,13 @@
+package com.codereview.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodereviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodereviewApplication.class, args);
+	}
+
+}
