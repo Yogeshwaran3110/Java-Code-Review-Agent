09631af67167e12e36775d4a6b37f8b74f7266e2
mohamedali3521 <mohamedali321k@gmail.com>
2026-09-19 23:22:52 +0530

    testing purpose

--- src/main/java/com/codereview/agent/Controller/HealthController.java
+++ src/main/java/com/codereview/agent/Controller/HealthController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
+
+    // testing purpose
     @GetMapping("/api/health")
     public String health(){
         return "API Health is running successfully";
